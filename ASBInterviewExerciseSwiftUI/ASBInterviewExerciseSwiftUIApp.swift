ab10f7c03e4482c5c7076faa6ea5d9561e86d703
Adam Feher <adam.feher@outlook.com>
2022-07-01 11:09:35 +0200

    Initial Commit

---
+++ ASBInterviewExerciseSwiftUI/ASBInterviewExerciseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASBInterviewExerciseSwiftUIApp.swift
+//  ASBInterviewExerciseSwiftUI
+//
+//  Created by Adam Feher on 2022. 07. 01..
+//
+
+import SwiftUI
+
+@main
+struct ASBInterviewExerciseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
